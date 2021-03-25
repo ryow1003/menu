da78704c9748111b635350e152918213ebfd4607
Ryow <64392776+ryow1003@users.noreply.github.com>
2021-03-25 19:02:16 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// ハンバーガーメニュー
+$(function() {
+  $('.menu').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.menu-after').addClass('active');
+      } else {
+          $('.menu-after').removeClass('active');
+      }
+  });
+});
+
+
+
