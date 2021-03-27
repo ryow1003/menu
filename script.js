7ef7dc0016325bd968042394c7978fe5f3dca7f8
Ryow <64392776+ryow1003@users.noreply.github.com>
2021-03-27 17:17:48 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+//ROADANIMATION
+window.onload = function() {
+  setTimeout(function() {
+    $('.load').fadeOut(1000);
+  }, 1000);
+}
+
 // ハンバーガーメニュー
 $(function() {
   $('.menu').click(function() {
-      $(this).toggleClass('active');
-
-      if ($(this).hasClass('active')) {
-          $('.menu-after').addClass('active');
-      } else {
-          $('.menu-after').removeClass('active');
-      }
+    $(this).toggleClass('active');
+    if ($(this).hasClass('active')) {
+    $('.menu-after').addClass('active');
+    } else {
+    $('.menu-after').removeClass('active');
+    }
   });
 });
 
