65dab93123ce21fb3728237d2ed83d00737e0ee3
Ryow <64392776+ryow1003@users.noreply.github.com>
2021-03-27 22:35:51 +0900

    Add files via upload

--- side/script.js
+++ side/script.js
@@ -9,3 +9,25 @@ $(function() {
     }
   });
 });
+
+
+// スクロールボタン
+jQuery(function() {
+  var pagetop = $('#scroll');
+  pagetop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+$('a[href^="#"]').click(function(){
+  var time = 500;
+  var href= $(this).attr("href");
+  var target = $(href == "#" ? 'html' : href);
+  var distance = target.offset().top;
+  $("html, body").animate({scrollTop:distance}, time, "swing");
+  return false;
+});
+});
