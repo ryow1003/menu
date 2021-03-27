01e60e347cc4a1189b66f24dbf1850a8c3f594bf
Ryow <64392776+ryow1003@users.noreply.github.com>
2021-03-27 22:34:20 +0900

    Add files via upload

--- about/script.js
+++ about/script.js
@@ -1,10 +1,3 @@
-//ROADANIMATION
-window.onload = function() {
-  setTimeout(function() {
-    $('.load').fadeOut(1000);
-  }, 1000);
-}
-
 // ハンバーガーメニュー
 $(function() {
   $('.menu').click(function() {
@@ -17,5 +10,24 @@ $(function() {
   });
 });
 
-
+// スクロールボタン
+jQuery(function() {
+  var pagetop = $('#scroll');
+  pagetop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+$('a[href^="#"]').click(function(){
+  var time = 500;
+  var href= $(this).attr("href");
+  var target = $(href == "#" ? 'html' : href);
+  var distance = target.offset().top;
+  $("html, body").animate({scrollTop:distance}, time, "swing");
+  return false;
+});
+});
 
